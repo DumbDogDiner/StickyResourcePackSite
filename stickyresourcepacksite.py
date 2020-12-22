6462f68dd12598c93da291d3c4790dced4177dce
Andrew Katz <aakatz3@gmail.com>
2020-12-22 14:38:31 -0700

    init css work, bs4

--- stickyresourcepacksite.py
+++ stickyresourcepacksite.py
@@ -1,14 +1,38 @@
 from flask import Flask
 import zipfile
 import dominate
-import bs4
+from bs4 import dammit,BeautifulSoup
 from dominate.tags import *
+import sys, os
 
 app = Flask(__name__)
 
 
+
+def generate_thumbnail(name):
+    if not(isinstance(name, str)):
+        raise ValueError('name must be of type str')
+    title = name.title()
+    name = title.replace(' ', '')
+    returned_div = div(id=name)
+    with returned_div:
+        attr(_class='thumbnail')
+        img(source=name.replace(' ', '') + '.png')
+        br()
+        p(title)
+    return returned_div
+
+
 @app.route("/")
 def main_page():
+    soup = BeautifulSoup(open('static/base.html'))
+
+
+
+
+
+
+
     doc = dominate.document(title="DumbDogDiner Resource Pack Builder")
     with doc.head:
         link(rel='stylesheet', href='static/style.css')
@@ -38,4 +62,5 @@ def dozip():
     return "derp"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    #app.run(host='0.0.0.0')
+    print(generate_thumbnail('test thingy'))
